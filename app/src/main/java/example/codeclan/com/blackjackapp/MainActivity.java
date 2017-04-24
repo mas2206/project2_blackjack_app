d5843c7f6a6087ae1e458d43b1399a40645285ad
Michael Shennan <m.shennan22@gmail.com>
2017-04-24 22:26:54 +0100

    starting on main activity, app crashes you when press the button

--- app/src/main/java/example/codeclan/com/blackjackapp/MainActivity.java
+++ app/src/main/java/example/codeclan/com/blackjackapp/MainActivity.java
@@ -2,12 +2,27 @@ package example.codeclan.com.blackjackapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Game game;
+    TextView dealersHandText;
+    TextView playersHandText;
+    TextView resultText;
+    Button buttonDealCards;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onDealCardsButtonClick(View view) {
+        game.play();
+    }
+
 }
