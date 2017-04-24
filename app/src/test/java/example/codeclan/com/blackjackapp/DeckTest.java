793a3fae2b3d54f205fa2224826ddbc5d2a46cfd
Michael Shennan <m.shennan22@gmail.com>
2017-04-24 20:48:01 +0100

    added decktest, all tests pass

---
+++ app/src/test/java/example/codeclan/com/blackjackapp/DeckTest.java
@@ -0,0 +1,32 @@
+package example.codeclan.com.blackjackapp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by user on 24/04/2017.
+ */
+
+public class DeckTest {
+
+    Deck deck;
+    Player player;
+
+    @Before
+    public void before() {
+        deck = new Deck(new ArrayList<Card>());
+        player = new Player("Michael", new Hand());
+    }
+
+    @Test
+    public void canDealCards() {
+        deck.dealCard(player);
+        deck.dealCard(player);
+        assertEquals(3, player.getHandValue());
+    }
+
+}
